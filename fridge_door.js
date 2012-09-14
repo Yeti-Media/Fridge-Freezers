9003439e91c367a0a7d5136bf6c23fb8900d6668
Salvador <salvador.og@gmail.com>
2012-09-14 18:38:15 -0300

    added subdomains for each fridge, now the db relation is through the name

--- fridge_door.js
+++ fridge_door.js
@@ -10,18 +10,21 @@ if (Meteor.is_client) {
   Template.new_fridge.events = {
     'click input.add': function () {
       var new_fridge = document.getElementById("new_fridge_name").value;
-      Fridges.insert({name: new_fridge});
+      Fridges.insert({name: new_fridge})
+      Session.set("selected_fridge", new_fridge);
+      location.hash = "#" + new_fridge;
     }
   };
 
 
   Template.fridge_info.maybe_selected = function () {
-    return Session.equals("selected_fridge", this._id) ? "selected" : '';
+    return Session.equals("selected_fridge", this.name) ? "selected" : '';
   };
 
   Template.fridge_info.events = {
     'click': function () {
-      Session.set("selected_fridge", this._id);
+      Session.set("selected_fridge", this.name);
+      location.hash = "#" + this.name;
     }
   };
 
@@ -31,11 +34,17 @@ if (Meteor.is_client) {
 
   Template.fridge.posts = function () {
     var fridge = Session.get("selected_fridge");      
-    return Posts.find({fridgeId: fridge})
+    return Posts.find({fridgeName: fridge})
   }
   Template.fridge.selected_fridge = function () {
+    var fridge_domain = location.hash.substr(1);
     var fridge = Session.get("selected_fridge");
-    return Fridges.findOne({_id: fridge});
+
+    return Fridges.findOne({name: fridge_domain});
+  };
+
+  Template.fridge.preview = function () {
+   // return document.getElementById("new_post_content").value;
   };
 
   Template.new_post.events = {
@@ -46,12 +55,14 @@ if (Meteor.is_client) {
       var radio = document.getElementById("post_type");  
       var post_type;
 
+      console.log(radio);
       for (i=0; i < radio.length;i++){
         if(radio[i].checked==true){
           post_type = radio[i].value;
       }};
 
-      Posts.insert({content: new_post, fridgeId: fridge, type: post_type });
+      post_type = 'text'
+      Posts.insert({content: new_post, fridgeName: fridge, type: post_type });
 
       // var new_post = document.getElementById("new_post_content").value,
       //     fridge = Session.get("selected_fridge"),
